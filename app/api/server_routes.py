1b624441be9658bfbc5db1eb7c7c29577d92ad30
JacobNicotra <nicotra1008@gmail.com>
2022-01-07 18:53:52 -0600

    server api skeleton complete CRUD

--- app/api/server_routes.py
+++ app/api/server_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-# from app.models import Server
+from app.models import db#, Server
 from sqlalchemy.exc import IntegrityError
 
 server_routes = Blueprint('channels', __name__)
@@ -33,3 +33,54 @@ def new_server():
     #     except IntegrityError as e:
     #         print(e)
     #         return jsonify('Database entry error'), 400
+
+
+@server_routes.route('/<int:server_id>')
+def get_server(server_id):
+    pass
+    # server = Server.query.filter(Server.id == server_id).first()
+    # if server:
+    #     return server
+    # else:
+    #     return jsonify("Server not found in database."), 404
+
+
+@server_routes.route('/<int:server_id>', methods=['PUT'])
+def update_server(server_id):
+    pass
+    
+    # title = None
+    # description = None
+    # image = None
+    # ownerId = None
+    # server = Server.query.filter(Server.id == server_id).first()
+
+    # if not request.args:
+    #         return jsonify('bad data'), 400
+    # elif server:
+    #     data = dict(request.args)
+    #     if data['title']:
+    #       server.title = data['title']
+    #     if data['description']:
+    #       server.description = data['description']
+    #     if data['image']:
+    #       server.image = data['image']
+    #     if data['ownerId']:
+    #       server.ownerId = data['ownerId']
+    #     db.session.commit
+          # return server
+
+    # else:
+    #     return jsonify("server not found in database."), 404
+
+
+@server_routes.route('/<int:server_id>', methods=['DELETE'])
+def delete_server(server_id):
+    pass
+    
+    # server = Server.query.filter(Server.id == server_id).first()
+    # if server:
+    #     db.session.delete(server)
+    #     return server
+    # else:
+    #     return jsonify("server not found in database."), 404
