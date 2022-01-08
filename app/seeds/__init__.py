8a1d33048e6ac2fa3ee88ba28cdc78b4e5cfad1a
Geoffrey Cox <geoffrey.pt.cox@gmail.com>
2022-01-07 17:24:40 -1000

    got basic backend working, need to update seed data

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,9 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .servers import seed_servers, undo_servers
+from .channels import seed_channels, undo_channels
+from .messages import seed_messages, undo_messages
+from .reactions import seed_reactions, undo_reactions
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -10,11 +14,19 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     seed_users()
+    seed_servers()
+    seed_channels()
+    seed_messages()
+    seed_reactions()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_reactions()
+    undo_messages()
+    undo_channels()
+    undo_servers()
     undo_users()
     # Add other undo functions here
