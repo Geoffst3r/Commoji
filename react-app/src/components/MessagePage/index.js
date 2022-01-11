9991bb7563243117d493d86a46d1db265c2cfee4
JacobNicotra <nicotra1008@gmail.com>
2022-01-11 15:58:46 -0600

    message react

--- react-app/src/components/MessagePage/index.js
+++ react-app/src/components/MessagePage/index.js
@@ -8,6 +8,8 @@ import { useHistory, useParams } from 'react-router';
 import { getMessages } from '../../store/messages';
 // import AddServerModal from '../AddServerModal';
 import './MessagePage.css'
+import MessageForm from './MessageForm';
+
 
 
 const Messages = () => {
@@ -17,41 +19,48 @@ const Messages = () => {
     const serverId = params.serverId;
     const channelId = params.channelId;
 
-// OBTAIN channelId !!!! maybe from state or url params ...
+    // OBTAIN channelId !!!! maybe from state or url params ...
 
     useEffect(() => {
         dispatch(getMessages(channelId))
     }, [dispatch])
 
-    // let messages = useSelector(state => {
-    //     return state.messages.messageArr
-    // })
+    let messages = useSelector(state => {
+        // console.log('_______STATE_______', state)
+        return state.messages.messageArr
+    })
 
-//     if (messages) {
-//         return (
-//             <>
+    console.log("messages in comp", messages)
 
-//                 <div className='MessageContainer'>
-//                     <ul className="MessageUl">
-//                         {messages.map(message => {
-//                             return (
-//                                 <li>
-//                                     <div><{message.messsage}</div>
-//                                     <div><{message.userId}</div>
-//                                 </li>
-//                             )
-//                         })}
+    if (messages) {
+        return (
+            <>
+
+                <div className='MessageContainer'>
+                    <ul className="MessageUl">
+                        {messages.map(message => {
+                            return (
+                                <>
+                                    <li>
+                                        <div>{message.message}</div>
+                                        <div>{message.username}</div>
+                                    </li>
 
 
-//                     </ul >
+                                </>
+                            )
+                        })}
 
-//                 </div>
-//             </>
-//         )
-//     }
-//     return ('no messages here!')
+
+                    </ul >
+                    <MessageForm />
+                </div>
+            </>
+        )
+    }
+    return ('no messages here!')
     console.log('params', params)
-        return (`no messages channel number ${channelId}`)
+    return (`no messages channel number ${channelId}`)
 
 }
 
--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -3,11 +3,13 @@ import thunk from 'redux-thunk';
 import session from './session';
 import serversReducer from './servers';
 import channelReducer from './channel';
+import messageReducer from './messages';
 
 const rootReducer = combineReducers({
   session,
   servers: serversReducer,
-  channels: channelReducer
+  channels: channelReducer,
+  messages: messageReducer
 
 });
 
