73f256b16fdc62285f646464f64dfb540a219abd
depash <depash2003@gmail.com>
2022-01-12 16:06:48 -0500

    finsihed making message  color changing

--- react-app/src/components/MessagePage/index.js
+++ react-app/src/components/MessagePage/index.js
@@ -14,7 +14,7 @@ import MessageForm from './MessageForm';
 const Messages = () => {
     const params = useParams();
     const dispatch = useDispatch()
-
+    const user = useSelector(state => state.session.user);
     const serverId = params.serverId;
     const channelId = params.channelId;
     useEffect(async () => {
@@ -24,7 +24,7 @@ const Messages = () => {
 
 
     const messages = useSelector(state => state.messages.messages);
-   
+
 
     if (messages) {
         return (
@@ -39,8 +39,8 @@ const Messages = () => {
                                     <>
                                         <li key={message.id}>
                                             {/* <div className='mesageContent'>{message.id}</div> */}
-                                            <div className='messageUser'>{messages[message].username}</div>
-                                            <div className='messageContent'>{messages[message].message}</div>
+                                            {user.id === messages[message].userId ? <><div className='userMessage messageUser'>{messages[message].username}</div><div className='userMessage messageContent'>{messages[message].message}</div></> :
+                                                <><div className='messageUser'>{messages[message].username}</div><div className='messageContent'>{messages[message].message}</div></>}
                                         </li>
                                     </>
                                 )
